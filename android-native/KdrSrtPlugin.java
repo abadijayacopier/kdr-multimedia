c341d426e86b378b5490b4ef83274e6b0f8f401c
abadijayacopier <109567275+abadijayacopier@users.noreply.github.com>
2026-09-23 08:18:55 +0700

    Fix native SRT plugin annotation import

--- android-native/KdrSrtPlugin.java
+++ android-native/KdrSrtPlugin.java
@@ -3,6 +3,7 @@ package com.kdrmultimedia.camera.srt;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "KdrSrt")
