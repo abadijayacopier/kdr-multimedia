30fc4b57c5bd74d985876e2615b8fd12ff6ac505
abadijayacopier <109567275+abadijayacopier@users.noreply.github.com>
2026-09-23 08:18:39 +0700

    Add Capacitor bridge for native Android SRT sender

---
+++ android-native/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kdrmultimedia.camera;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+import com.kdrmultimedia.camera.srt.KdrSrtPlugin;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(KdrSrtPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
