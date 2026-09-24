8d978735c32dbaa4950915abe8b8dfd589d03a7d
abadijayacopier <109567275+abadijayacopier@users.noreply.github.com>
2026-09-24 21:18:54 +0700

    feat(android): enable sensor auto rotation

--- android-native/MainActivity.java
+++ android-native/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kdrmultimedia.camera;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.kdrmultimedia.camera.srt.KdrSrtPlugin;
@@ -9,5 +10,6 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(KdrSrtPlugin.class);
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
     }
 }
